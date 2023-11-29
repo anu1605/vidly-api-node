e4c376cac564c82bb2fe6e4efd0ec2da84c186fb
Anushka Tyagi <anushka.tyagi161998@gmail.com>
2023-11-29 12:26:34 +0530

    improved import statements

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const winston = require("winston");
-const express = require("express");
-const config = require("config");
+import { info } from "winston";
+import express from "express";
+import { get } from "config";
 const app = express();
 
 require("./startup/logging")();
@@ -10,9 +10,7 @@ require("./startup/db")();
 require("./startup/config")();
 require("./startup/validation")();
 
-const port = process.env.PORT || config.get("port");
-const server = app.listen(port, () =>
-  winston.info(`Listening on port ${port}...`)
-);
+const port = process.env.PORT || get("port");
+const server = app.listen(port, () => info(`Listening on port ${port}...`));
 
-module.exports = server;
+export default server;
